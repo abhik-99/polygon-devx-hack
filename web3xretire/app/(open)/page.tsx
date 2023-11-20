abbcf895d2adef5edf245bbd10f3db907f989b0b
Sumon Roy <sumon.roy@thewitslab.com>
2023-11-21 01:12:03 +0530

    feat : refs updated for team and tech stack section

--- web3xretire/app/(open)/page.tsx
+++ web3xretire/app/(open)/page.tsx
@@ -5,8 +5,8 @@ import { web3AuthInstance } from "../../lib/utils/web3auth";
 import { publicKeyToAddress } from "../../lib/utils/publicKeyToAddress";
 import HeroSection from "../sections/landing/HeroSection/HeroSection";
 import AboutSection from "../sections/landing/AboutSection/AboutSection";
-import TeamSection from "../sections/landing/TeamSection";
-import TechStackSection from "../sections/landing/TechStackSection";
+import TeamSection from "../sections/landing/TeamSection/TeamSection";
+import TechStackSection from "../sections/landing/TechStackSection/TechStackSection";
 
 export default function Home() {
   return (
